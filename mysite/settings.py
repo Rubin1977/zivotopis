39b95a73df7ad7a5632d585f9f513303a5975612
RUBIN1977 <ruzbacky@yahoo.com>
2023-09-19 14:26:43 +0200

    Formuler without captha

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from django_recaptcha import RECAPTCHA_PUBLIC_KEY, RECAPTCHA_PRIVATE_KEY
+#from django_recaptcha import RECAPTCHA_PUBLIC_KEY, RECAPTCHA_PRIVATE_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,17 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--8xb9ke-(n_s!*buw%g%om#=iy8rvkt@vo-m=dvax2uj8wrgdo'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'eu.pythonanywhere.com' ]
-RECAPTCHA_PUBLIC_KEY = '6LcZjm4nAAAAANiS1LzGOAwMjhOikg5S-OOWqPbW'
-RECAPTCHA_PRIVATE_KEY = '6LcZjm4nAAAAAAvTXjzph9_RaqVX-BPkcWhKrOr0'
-RECAPTCHA_REQUIRED = True
-#RECAPTCHA_TESTING = False
-#SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+
+
+
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'

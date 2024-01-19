9b7cc7c1de59300bdebbbc30345cac74ea864b2b
RUBIN1977 <ruzbacky@yahoo.com>
2024-01-19 15:23:42 +0100

    javascrpit is working

--- zivotopis/static/js/main.js
+++ zivotopis/static/js/main.js
@@ -1,4 +1,9 @@
 alert("Vitajte na mojej stránke!");
 window.onload = function () {
-    document.body.style.backgroundColor = "lightblue";
+    var myButton = document.getElementById("myButton");
+    // Pridáme event listener, aby sme zachytili kliknutie na tlačidlo
+    myButton.addEventListener("click", function () {
+        // Zmeníme text v elemente s ID "myText" na uvítaciu správu
+        document.getElementById("myText").innerHTML = "Vitajte na mojej stránke!";
+    });
 }

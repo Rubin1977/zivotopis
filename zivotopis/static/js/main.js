9e239454f962fd5271b544e3dcc1e92bf1df24d9
RUBIN1977 <ruzbacky@yahoo.com>
2024-01-20 20:27:34 +0100

    header changes

--- zivotopis/static/js/main.js
+++ zivotopis/static/js/main.js
@@ -4,6 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     myButton.addEventListener("click", function () {
         // Zmeníme text v elemente s ID "myText" na uvítaciu správu
-        myText.innerHTML = "Vitajte na mojej stránke!";
+        myText.innerHTML = "Pozadie tlačidla sa zmenilo :-)";
+        var randomColor = getRandomColor();
+        // Nastavíme novú farbu pozadia tlačidla
+        myButton.style.backgroundColor = randomColor;
+
+        // Funkcia na generovanie náhodnej farby v hex formáte
+        function getRandomColor() {
+            var letters = "0123456789ABCDEF";
+            var color = "#";
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     });
 });

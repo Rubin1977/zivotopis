6467eea5c3aae01980f3f675caa538344414cb73
RUBIN1977 <ruzbacky@yahoo.com>
2024-02-14 18:14:00 +0100

    Settings for email

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,8 +36,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rubin7y@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehlmfvwamszlrluh'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Application definition

41892f275673c8d630005f3d8f4fe72390efa9ee
RUBIN1977 <ruzbacky@yahoo.com>
2024-02-18 15:35:06 +0100

    Enviroment variables

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+load_dotenv()
 #SECRET_KEY = 'django-insecure--8xb9ke-(n_s!*buw%g%om#=iy8rvkt@vo-m=dvax2uj8wrgdo'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 

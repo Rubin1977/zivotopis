680e37bd4f1e724fdcd0db2e27dbcd8c7f8e9300
RUBIN1977 <ruzbacky@yahoo.com>
2024-02-21 18:16:47 +0100

    Color changes in header and footer

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,8 +40,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = 'rubin7y@gmail.com'
-#EMAIL_HOST_PASSWORD = 'fdkldfdkjlfs'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 

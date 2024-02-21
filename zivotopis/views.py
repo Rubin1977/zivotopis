1c4b0cde7324371be6c1226164f3009e0e665fdb
RUBIN1977 <ruzbacky@yahoo.com>
2024-02-21 20:55:53 +0100

    view for send_email change

--- zivotopis/views.py
+++ zivotopis/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import redirect
-from django.shortcuts import render
-from django.utils import timezone, format_html #formátuje hlavičku
+from django.shortcuts import render, redirect
+from django.utils import timezone, #format_html #formátuje hlavičku
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
 from .forms import EmailForm
 from django.contrib import messages
 from django.core.mail import send_mail

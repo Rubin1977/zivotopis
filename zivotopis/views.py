7bdf0144ec130d44728f27069b08e3f8af16837a
RUBIN1977 <ruzbacky@yahoo.com>
2024-03-03 13:15:07 +0100

    images in post list

--- zivotopis/views.py
+++ zivotopis/views.py
@@ -11,7 +11,8 @@ from django.core.mail import send_mail
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'zivotopis/post_list.html', {'posts': posts})
+    images = Image.objects.filter(post=post)
+    return render(request, 'zivotopis/post_list.html', {'posts': posts, 'images': images})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)

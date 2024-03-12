65445f3f9ec28f003de66f909b1c49383f8d4535
RUBIN1977 <ruzbacky@yahoo.com>
2024-03-12 19:28:38 +0100

    modal window to size the pictures

--- zivotopis/static/js/main.js
+++ zivotopis/static/js/main.js
@@ -24,16 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
 // Kódový blok 2: Zväčšenie obrázkov po kliknutí
 
 // Získať všetky obrázky s triedou 'post-image'
-document.addEventListener("DOMContentLoaded", function () {
-    var images = document.querySelectorAll('.post-image');
+//document.addEventListener("DOMContentLoaded", function () {
+    //var images = document.querySelectorAll('.post-image');
     // Pridáme onClick udalosť každému obrázku
-    images.forEach(function (image) {
-        image.addEventListener('click', function () {
+    //images.forEach(function (image) {
+        //image.addEventListener('click', function () {
             // Ak obrázok má triedu 'zoomed-in', odstráňme ju, inak ju pridajte
-            this.classList.toggle('zoomed-in');
-        });
-    });
-});
+            //this.classList.toggle('zoomed-in');
+        //});
+    //});
+//});
 
 document.addEventListener("DOMContentLoaded", function () {
     var images = document.querySelectorAll('.post-image');

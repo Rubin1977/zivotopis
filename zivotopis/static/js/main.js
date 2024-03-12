aca717bec11271526b0b93b87cbb180c74f9c158
RUBIN1977 <ruzbacky@yahoo.com>
2024-03-12 18:33:37 +0100

    modal window

--- zivotopis/static/js/main.js
+++ zivotopis/static/js/main.js
@@ -35,25 +35,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Funkcia na zobrazenie modálneho okna s obrázkom
-//function showModal(imageSrc) {
-    // Odkaz na modálne okno
-    //var modal = document.getElementById("zoomed-in-modal");
-    // Odkaz na obrázok v modálnom okne
-    //var zoomedImage = document.getElementById("zoomed-in-image");
+//Funkcia na zobrazenie modálneho okna s obrázkom
+function showModal(imageSrc) {
+    //Odkaz na modálne okno
+    var modal = document.getElementById("zoomed-in-modal");
+    //Odkaz na obrázok v modálnom okne
+    var zoomedImage = document.getElementById("zoomed-in-image");
     // Nastaviť obrázok v modálnom okne
-    //zoomedImage.src = imageSrc;
+    zoomedImage.src = imageSrc;
     // Zobraziť modálne okno
-    //modal.style.display = "block";
-//}
+    modal.style.display = "block";
+}
 
 // Udalosť na kliknutie mimo obrázka (skryje modálne okno)
-//window.onclick = function (event) {
-    //var modal = document.getElementById("zoomed-in-modal");
-    //if (event.target == modal) {
-    //    modal.style.display = "none";
-    //}
-//}
+window.onclick = function (event) {
+    var modal = document.getElementById("zoomed-in-modal");
+        if (event.target == modal) {
+            modal.style.display = "none";
+    }
+}
 
 // úvodný kód pre galériu
 document.addEventListener("DOMContentLoaded", function () {

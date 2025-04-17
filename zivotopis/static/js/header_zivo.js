f5cc694a9822873c5be6df8dcfe2724e521b75fe
Rastislav Ružbacký <ruzbacky@yahoo.com>
2025-04-17 20:20:41 +0200

    improve main and about me

--- zivotopis/static/js/header_zivo.js
+++ zivotopis/static/js/header_zivo.js
@@ -7,3 +7,8 @@ window.addEventListener("resize", function () {
     const header = document.querySelector(".fixed-header");
     document.body.style.paddingTop = `${header.offsetHeight}px`;
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+    main.style.paddingTop = `${header.offsetHeight}px`;
+});

cccacf4587137a4217a45a467625a4b410492553
Rastislav Ružbacký <ruzbacky@yahoo.com>
2025-05-14 17:54:23 +0200

    improve CV

--- zivotopis/static/js/header_zivo.js
+++ zivotopis/static/js/header_zivo.js
@@ -1,14 +1,29 @@
 window.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector(".fixed-header");
-    document.body.style.paddingTop = `${header.offsetHeight}px`;
+    const main = document.querySelector("main");
+
+    if (header) {
+        document.body.style.paddingTop = `${header.offsetHeight}px`;
+    }
+
+    if (header && main) {
+        main.style.paddingTop = `${header.offsetHeight}px`;
+    }
 });
 
 window.addEventListener("resize", function () {
     const header = document.querySelector(".fixed-header");
-    document.body.style.paddingTop = `${header.offsetHeight}px`;
+
+    if (header) {
+        document.body.style.paddingTop = `${header.offsetHeight}px`;
+    }
 });
-document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector("header");
-    const main = document.querySelector("main");
-    main.style.paddingTop = `${header.offsetHeight}px`;
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector(".fixed-header");
+    if (window.scrollY > 50) {
+        header.classList.add("scroll"); // Pridá triedu pri posunutí
+    } else {
+        header.classList.remove("scroll"); // Odstráni triedu, keď si hore
+    }
 });

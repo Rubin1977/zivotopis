24a37ea27bc9eacfcd3aa798b0b9486d19f5d3f7
Rastislav Ružbacký <ruzbacky@yahoo.com>
2025-09-10 19:12:49 +0200

    Switch to MySQL database

--- mysite/settings.py
+++ mysite/settings.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RastislavRuzback$Rastislav',
+        'USER': 'RastislavRuzback',
+        'PASSWORD': 'MySql13',
+        'HOST': 'RastislavRuzbacky.mysql.eu.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 

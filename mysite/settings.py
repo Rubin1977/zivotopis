373741aecc1ca2288b5d3b25b8b8197d610ee0db
Rubin1977 <ruzbacky@yahoo.com>
2025-10-21 14:59:00 +0200

    Upravené tlačidlo prihlásenia a navbar pre DEBUG

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ load_dotenv(BASE_DIR / ".django_env")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com', 'eu.pythonanywhere.com' ]
 

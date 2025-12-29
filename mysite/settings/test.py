15aa025c64f306421f10915ce93aa5593bb027c6
Rubin1977 <ruzbacky@yahoo.com>
2025-12-29 17:45:59 +0100

    different settings for PA db and local db

--- mysite/settings/test.py
+++ mysite/settings/test.py
@@ -1,34 +1,18 @@
 # settings_test.py
 from .base import *
 import os
-import socket
+
 
 print("OK – Bežíme v testovacom režime (mysite/settings/test.py)")
 
-# Rozlíšime lokálne vs. remote podľa hostname
-HOSTNAME = socket.gethostname()
 
-# Lokálne testy → SQLite
-if "pythonanywhere" not in HOSTNAME:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'test_db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
     }
+}
 
-# Remote testy → MySQL test DB
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'RastislavRuzback$Rastislav',
-            'USER': 'RastislavRuzback',
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': 'RastislavRuzbacky.mysql.eu.pythonanywhere-services.com',
-            'PORT': '3306',
-        }
-    }
 
 DEBUG = True
 
